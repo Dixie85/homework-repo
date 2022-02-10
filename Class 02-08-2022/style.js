006e561501f1fe8b11f3975a50de8450c82329b5
Dixie85 <98489775+Dixie85@users.noreply.github.com>
2022-02-10 03:48:42 +0100

    Star Wars Finished

--- Class 02-08-2022/style.js
+++ Class 02-08-2022/style.js
@@ -11,6 +11,7 @@ const memory = {
     starshipsPrevious: null,
     starshipsNext: null,
 };
+    
 
 function getData(url) {
     return fetch(url)
@@ -33,21 +34,27 @@ function starshipsMemory(r) {
 }
 
 function showData(data){
+   const people = ["name", "height", "mass", "birth_year", "gender", "films"];
+   const ships = [ "name", "model", "manufacturer", "cost_in_credits", "passengers", "starship_class"];
 
     if(data === memory.peopleData){
         const pTitle = Object.keys(data[0]).filter(key => {
-            if(typeof data[0][key] === "string"){
-                return key
+            for(let pip of people){
+                if(key === pip){
+                   return key
+                }
+                continue
             }
         });
         const tr = createElem("tr");
-        $(tr).html(pTitle.reduce((str, key) => {
-            return str += `<th>${key}</th>`
-        },""));
+        $(tr).html(pTitle.reduce((str, key) => str += `<th>${key}</th>`,""));
         $("#peopleTitles").html(tr);
         console.log(tr)
         $("#peopleData").html(data.reduce((str, char)=>{
             const columns = pTitle.reduce((str, key) => {
+                if(key === "films"){
+                    return str += `<td>${char[key].length}</td>`
+                };
                 return str += `<td>${char[key]}</td>`;
             }, "");
             return str += `<tr>${columns}</tr>`
@@ -56,18 +63,25 @@ function showData(data){
 
     if(data === memory.starshipsData) {
         const shTitle = Object.keys(data[0]).filter(key => {
-            if(typeof data[0][key] === "string"){
-                return key
+            for(let ship of ships){
+                if(key === ship){
+                   return key
+                }
+                continue
             }
         });
         const tr = createElem("tr");
-        $(tr).html(shTitle.reduce((str, key) => {
-            return str += `<th>${key}</th>`
-        },""));
+        $(tr).html(shTitle.reduce((str, key) => str += `<th>${key}</th>`,""));
         $("#starshipsTitles").html(tr);
         console.log(tr)
         $("#starshipsData").html(data.reduce((str, char)=>{
             const columns =shTitle.reduce((str, key) => {
+                if(key === "passengers"){
+                    if (isNaN(parseInt(char[key]))){
+                        return str +=`<td>No Info</td>`
+                    };
+                    return str += `<td>${parseInt(char[key])}</td>`
+                };
                 return str += `<td>${char[key]}</td>`;
             }, "");
             return str += `<tr>${columns}</tr>`
@@ -89,13 +103,14 @@ $(document).ready(function() {
     const starshipsNextBtn = $("#starshipsNext").hide();
     const starships = $("#starships");
     const person = $("#person");
-    
+    const tables = $("table").hide();
     
     person.click(e => memory.peopleUrl && getData(memory.peopleUrl)
     .then(r => peopleMemory(r))
     .then(r => showData(memory.peopleData))
     .then(peopleNextBtn.show())
     .then(person.hide())
+    .then(tables.show())
     );
 
     starships.click(e => memory.starshipsUrl && getData(memory.starshipsUrl)
@@ -103,6 +118,7 @@ $(document).ready(function() {
     .then(r => showData(memory.starshipsData))
     .then(starshipsNextBtn.show())
     .then(starships.hide())
+    .then(tables.show())
     );
     
     peoplePreviousBtn.click(e => memory.peoplePrevious && getData(memory.peoplePrevious)
